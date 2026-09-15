02312838c18b2d09f20022b2066a8c3de02487a6
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-09-15 14:01:45 -0600

    Update dgt_depositos.py

--- pjecz_ursa_maior_cli_typer/commands/dgt_depositos.py
+++ pjecz_ursa_maior_cli_typer/commands/dgt_depositos.py
@@ -7,7 +7,7 @@ from rich.table import Table
 from sqlalchemy import select
 from typer import Typer
 
-from pjecz_ursa_maior_cli_typer.models.dgt_depositos import DGTDeposito
+from pjecz_ursa_maior_cli_typer.models.dgt_depositos import DgtDeposito
 from pjecz_ursa_maior_cli_typer.utils.database import get_database
 
 app = Typer(help="DGT Depósitos comandos")
