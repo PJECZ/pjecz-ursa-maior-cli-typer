e298a407cff6b1a15a44680726b67d985f9b2452
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-09-21 10:15:15 -0600

    Corregi comandos y agregue proposito

--- pjecz_ursa_maior_cli_typer/commands/dgt_depositos.py
+++ pjecz_ursa_maior_cli_typer/commands/dgt_depositos.py
@@ -19,10 +19,11 @@ def consultar(offset: int = 0, limit: int = 40):
     console = Console()
     console.print("Consultando DGT depósitos...")
     db = get_database()
-    stmt = select(DgtDeposito.clave, DgtDeposito.descripcion).order_by(DgtDeposito.clave).offset(offset).limit(limit)
+    stmt = select(DgtDeposito.clave, DgtDeposito.descripcion, DgtDeposito.proposito).order_by(DgtDeposito.clave).offset(offset).limit(limit)
     tabla = Table(title="DGT Depósitos")
     tabla.add_column("Clave", header_style="green", no_wrap=True)
     tabla.add_column("Descripción", header_style="green")
+    tabla.add_column("Propósito", header_style="green")
     for item in db.execute(stmt):
-        tabla.add_row(item.clave, item.descripcion)
+        tabla.add_row(item.clave, item.descripcion, item.proposito)
     console.print(tabla)
